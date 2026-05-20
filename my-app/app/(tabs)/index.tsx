a8414b58903a8a18726584d10a997bdb336c7329
abidahakime <abidastudy@gmail.com>
2026-05-20 07:19:24 +0430

    feat: project setup,nativwind, routing, tab navigation

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
-
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import { styled } from "nativewind";
+const SafeAreaView = styled(RNSafeAreaView);  
 export default function App() {
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+    <SafeAreaView  className="flex-1 p-5 bg-background">
+      <Text className="text-xl font-bold bg- text-blue-500">
         Welcome to Nativewind!
       </Text>
       <Link
@@ -34,6 +37,6 @@ export default function App() {
       >
         Claude Max Subscription
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
