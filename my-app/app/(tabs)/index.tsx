d7eccc64836559224eb4925a888c48ce132966bf
abidahakime <abidastudy@gmail.com>
2026-05-23 06:38:17 +0430

    implementsing fonts

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -7,36 +7,28 @@ export default function App() {
 
   return (
     <SafeAreaView  className="flex-1 p-5 bg-background">
-      <Text className="text-xl font-bold bg- text-blue-500">
-        Welcome to Nativewind!
+      <Text className="text-5xl fotext-blue-50 font-sans-extrabold">
+        Home
       </Text>
       <Link
         href={"/onboarding"}
-        className=" mt-4 rounded bg-primary text-white p-4"
+        className=" mt-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         Go to Onboarding
       </Link>
       <Link
         href={"/(auth)/sign-in"}
-        className=" mt-4 rounded bg-primary text-white p-4"
+        className=" mt-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         Go to Sign in
       </Link>
       <Link
         href={"/(auth)/sign-up"}
-        className=" mt-4 rounded bg-primary text-white p-4"
+        className=" mt-4 font-sans-bold rounded bg-primary text-white p-4"
       >
         Go to Sign up
       </Link>
-      <Link href={"/subscriptions/spotify"}>Spotify subscription</Link>
-      <Link
-        href={{
-          pathname: "/subscriptions/[id]",
-          params: { id: "claude" },
-        }}
-      >
-        Claude Max Subscription
-      </Link>
+     
     </SafeAreaView>
   );
 }
--- my-app/app/_layout.tsx
+++ my-app/app/_layout.tsx
@@ -1,5 +1,23 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
 import "./global.css";
+import { useEffect } from "react";
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    PlusJakartaSans_400Regular: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    PlusJakartaSans_700Bold: require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    PlusJakartaSans_800ExtraBold: require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    PlusJakartaSans_600SemiBold: require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    PlusJakartaSans_500Medium: require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    PlusJakartaSans_300Light: require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+  });
+ useEffect(()=>{
+   if(fontsLoaded){
+    SplashScreen.hideAsync()
+   }
+ },[fontsLoaded]  )
+ if(!fontsLoaded){
+  return null
+ }  
   return <Stack screenOptions={{ headerShown: false }} />;
 }
